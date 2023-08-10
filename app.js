ab7c9b0f2cd9acd973a0e5db059036357d93ba78
Belle Xu <bellex22@protonmail.com>
2023-08-09 20:06:26 -0400

    finished routes

--- app.js
+++ app.js
@@ -6,14 +6,48 @@ app.use(express.json());
 
 
 app.get("/items", function (req, res) {
-  return res.json(items);
+  return res.json({ items });
 });
 
-app.post("/items", function(req, res) {
-  request = req.body;
+app.post("/items", function (req, res) {
+  let request = req.body;
   items.push(request);
-  return res.json({added: request});
+  return res.json({ added: request });
 
+});
+
+app.get("/items/:name", function (req, res) {
+
+  let name = req.params.name;
+  let item = items.find(item => item.name === name);
+
+  return res.send(item);
+});
+
+app.patch("/items/:name", function (req, res) {
+
+  let request = req.body;
+  console.log("req param name", req.params.name);
+  let itemIndex = items.findIndex(item => item.name === req.params.name);
+
+
+  items[itemIndex] = request;
+
+
+  return res.json({ updated: request });
+});
+
+app.delete("/items/:name", function (req, res) {
+
+  let request = req.body;
+  console.log("req param name", req.params.name);
+  let itemIndex = items.findIndex(item => item.name === req.params.name);
+
+
+  items.splice(itemIndex, 1);
+
+
+  return res.json({ message: "Deleted" });
 });
 
 
